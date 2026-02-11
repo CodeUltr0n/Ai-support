58c01f3a3544d888d012913c62ff361c0257f6f3
ketan chokkara <chokkaraketanrao@gmail.com>
2026-02-11 20:34:08 +0530

    added-server-traffic-controll

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,19 @@ import { Hono } from 'hono';
 import chatRoute from './routes/chat.route.js';
 import { cors } from 'hono/cors';
 
+import { rateLimiter } from "hono-rate-limiter";
+import { type Context } from "hono";
+
 const app = new Hono();
+
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: (c: Context) => c.req.header("cf-connecting-ip") || "", // Method to generate custom identifiers for clients.
+});
 app.use('*',cors({origin:'*'}));
+app.use(limiter);
 app.route('/api/chat', chatRoute);
 
 export default app;
